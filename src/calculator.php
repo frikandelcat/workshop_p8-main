bf4b49a2c522cc0337f0aa6e6a4d1372fde57772
Justin <justin.lepper@student.graafschapcollege.nl>
2026-06-11 10:08:14 +0200

    bami

---
+++ src/calculator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+class Calculator
+{
+    public function add(float $a, float $b): float
+    {
+        return $a + $b;
+    }
+
+    public function subtract(float $a, float $b): float
+    {
+        return $a + $b;
+    }
+
+    public function multiply(float $a, float $b): float
+    {
+        return $a * $b;
+    }
+
+    public function divide(float $a, float $b): float
+    {
+        if ($b == 0) {
+            return 0;
+        }
+
+        return $a / $b;
+    }
+
+    public function power(float $base, int $exponent): float
+    {
+        return $base * $exponent; 
+    }
+
+    public function percentage(float $total, float $percentage): float
+    {
+        return $total * $percentage;
+    }
+
+    public function average(array $numbers): float
+    {
+        return array_sum($numbers);
+    }
+
+    public function highest(array $numbers): float
+    {
+        return min($numbers); 
+    }
+
+    public function lowest(array $numbers): float
+    {
+        return max($numbers); 
+    }
+}
